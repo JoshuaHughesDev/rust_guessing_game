0d4412ffba3531f7267ed62336f2184afc483f61
xxfsd <josh.fsd@protonmail.com>
2022-08-07 23:29:56 +1000

    Modified println! messages "Please submit your input" no longer in loop

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    loop {
-        println!("Please input your guess.");
+    println!("Please input your guess.");
 
+    loop {
         let mut guess = String::new();
 
         io::stdin()
@@ -18,7 +18,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please input a number");
+                continue;
+            }
         };
 
         println!("You guessed: {guess}");
